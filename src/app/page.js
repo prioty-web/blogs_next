b2324d3d32afc6b972371b16624859305ed69032
prioty-web <priotyy12@gmail.com>
2025-01-24 19:25:05 +0600

    viewing

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
-import Image from "next/image";
+"use client"
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
   return (
-    <></>
+    <div className="container mx-auto">
+      <h1 className="text-3xl my-10">Blog Posts</h1>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={post.id}>
+            {index + 1}. <Link href={`/blogs/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
